6fa8ea64945fc3bd3656f866cf6c309093ceba7d
Jennifer Eckenrode <j.eckenrode@me.com>
2023-04-18 00:32:33 -0500

    fixed prevent default on listener

--- script.js
+++ script.js
@@ -1,41 +1,55 @@
 // To-dos:
-// bootstrap cards, bootstrap list items to make the app look nice
-//grab date-times "dt" with unix by adding 000 for seconds
+// bootstrap list items to make the app look nice
+//grab date-times "dt" 
 // moment({unix}, "X") .format("MM/DD/YYYY HH:mm:ss")
-//then 3 HOURS from now same format^^ by skipping some index values
 //[0] is 9 am, [8] is 9am next day same time, [16] third day, [24] fourth day, [32] fifth day
 // grabbing values for weather icon temp1.list[0].weather[0].icon, then plug in value '01d' into the get icon URL and then plug in href link!
 // create event listener, put whole fetch inside of listener
 // grab input from BUTTON ITSELF with event delegation: event.target.textContent to grab text of the 'City"
 
-var searchForm = document.querySelector('#search-form');
-var searchButton = document.querySelector('#search-button');
+var APIKey = "533fc048d1bc1b13a13ca3f27b8a2fed";
+var q = "";
+var now = moment();
+//Date and time formate for header
+var currentDate = now.format('DD MM YYYY || HH:mm:ss');
+$("#currentDay").text(currentDate);
 
-searchButton.addEventListener('click', function(event) {
-    // Prevent the default behavior of the button (i.e., submitting the form)
+$("#search-button").on("click", function (event) {
+    // Preventing the button from trying to submit the form
     event.preventDefault();
 
 })
+
+
+function getWeather(q) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + q + "&units=imperial&appid=" + APIKey;
+
+    $.ajax({
+        // gets the current weather info
+        url: queryURL,
+        method: "GET",
+        error: (err => { //If API through error then alert 
+            alert("Your city was not found. Check your spelling or enter a city code")
+            return;
+          })
+        })
+        .then(function (response) {
+        console.log(response)
+        $(".cityList").empty()
+        $("#days").empty()
+
+        var image = $('<img class="imgsize">').attr('src', 'http://openweathermap.org/img/w/' + response.weather[0].icon + '.png');
+
+            
+        });
+    
+
+
+
+          //////
 var city = document.querySelector('#city-input').value;
 
-fetch("http://api.openweathermap.org/geo/1.0/direct?q=seoul&limit=5&appid=533fc048d1bc1b13a13ca3f27b8a2fed")
-    .then(response => response.json())
-    .then(citiesFound => { 
-        //the cities that render in console from array
-        let firstCity = citiesFound[0]; 
-        // the first city listed in the array, [0] will be the city I receive latitude and longitude coordinates from!
-        console.log(firstCity.lat);
-        console.log(firstCity.lon);
 
         return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&appid=533fc048d1bc1b13a13ca3f27b8a2fed`) 
         
-        // we can continue chaining off of this response object, first fetch depends on the second fetch 
-     })
-    
-
-        // we will then pull data from this stream
-    .then(response => response.json())
-    .then(data => {
-        // data being logged from first fetch
-        console.log(data);
-     })
+    }
